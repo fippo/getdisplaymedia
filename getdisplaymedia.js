6fd6f2f15dc1e1bb7e668bf8b4aec0ade1c840d3
Philipp Hancke <fippo@andyet.net>
2018-06-15 09:14:19 +0200

    initial commit
    still very much WIP

---
+++ getdisplaymedia.js
@@ -0,0 +1,56 @@
+var adapter = require('webrtc-adapter');
+var browser = adapter.browserDetails.browserName;
+
+var firefoxDefaultSurface;
+var chromeCallback;
+module.exports = {
+    shimgetDiplayMedia(window) {
+        if (!window.navigator || 'getDisplayMedia' in window.navigator) {
+            return;
+        }
+        if (!(browser === 'chrome' || browser === 'firefox')) {
+            return;
+        }
+        navigator.getDisplayMedia = function(constraints) {
+            if (browser === 'firefox') {
+                if (!firefoxDefaultSurface) {
+                    // throw an error indicating a choice must be made.
+                    return;
+                }
+                if (constraints.video) {
+                    constraints.video.mediaSource = firefoxDefaultSurface;
+                    return navigator.mediaDevices.getUserMedia(constraints);
+                } else {
+                    // throw an error? What is the default.
+                }
+            } else if (browser === 'chrome')
+                if (!chromeCallback) {
+                    // throw an error indicating a choice must be made.
+                    return;
+                }
+                return chromeCallback.then(function(sourceId) {
+                    // TODO: empty sourceId means user aborted. Throw an error.
+                    constraints = {video: {mandatory: {
+                        chromeMediaSource: 'desktop';
+                        maxWidth: window.screen.width,
+                        maxHeight: window.screen.height,
+                        maxFrameRate: 3
+                    }}};
+                    return navigator.mediaDevices.getUserMedia({
+                            mandatory: {
+                                chromeMediaSource: 'desktop',
+                            }
+                    });
+                });
+            }
+        }
+    },
+
+    setFirefoxDefaultSurface(surface) {
+        firefoxDefaultSurface = surface;
+    },
+
+    setChromeCallback(cb) {
+        chromeCallback = cb;
+    }
+};
